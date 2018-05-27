99ad626b2a6bad785388eba30b4dd2a75b84590e
Zak Kurka <zakkurka@gmail.com>
2018-05-27 17:21:20 -0500

    Tell user when no spreads are found

--- src/components/result.js
+++ src/components/result.js
@@ -1,7 +1,11 @@
-import { join } from 'lodash-es';
+import { join, isEmpty } from 'lodash-es';
 import { SpacedBlock } from './spaced-block';
 
 const Result = ({ result }) => {
+  if (isEmpty(result)) {
+    return <div style={{ marginTop: '15px' }}>No results found!</div>
+  }
+
   const { seed, time, isFirstBlockNewer } = result;
   const { pid, nature, ivs, shinySeed } = seed;
   
