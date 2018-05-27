5e88fe089c6107c29d69437c63243ecae3295ffd
Zak Kurka <zakkurka@gmail.com>
2018-05-27 08:11:29 -0500

    Add alert for latest save issue

--- src/components/result.js
+++ src/components/result.js
@@ -1,13 +1,21 @@
+import { join } from 'lodash-es';
 import { SpacedBlock } from './spaced-block';
 
-const Result = ({ seed, time }) => {
+const Result = ({ result }) => {
+  const { seed, time, isFirstBlockNewer } = result;
   const { pid, nature, ivs, shinySeed } = seed;
+  
+  const saveMessage = join([
+    'Your latest save is in the wrong space',
+    'please save once before attempting this'
+  ], ' - ');
 
   return (
-    <div>
+    <div style={{ marginTop: '15px' }}>
       <SpacedBlock>{time}</SpacedBlock>
       <SpacedBlock>Seed: {shinySeed}, PID: {pid}, Nature: {nature}</SpacedBlock>
       <SpacedBlock>IVs: {ivs}</SpacedBlock>
+      <SpacedBlock>{isFirstBlockNewer ? saveMessage : ''}</SpacedBlock>
     </div>
   );
 }
